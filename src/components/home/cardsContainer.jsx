eb393a156ece289d007a9f230930d69af908cf18
Innis <1575270674@qq.com>
2023-01-03 06:57:17 +0800

    feat: add media query

--- src/components/home/cardsContainer.jsx
+++ src/components/home/cardsContainer.jsx
@@ -10,7 +10,7 @@ import { useLocation } from 'react-router-dom';
 
 
 export function CardsContainer() {
-    const [filterVisible, setFilterVisible] = useState(true);
+    const [filterVisible, setFilterVisible] = useState(false);
 
     let entries;
 
