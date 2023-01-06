23c3aeeda8d8138780b4b789f3225b9d95405fd7
Innis <1575270674@qq.com>
2023-01-07 01:07:48 +0800

    feat: big font for room info

--- src/components/room/topicArea.jsx
+++ src/components/room/topicArea.jsx
@@ -1,68 +1,91 @@
 import "./topicArea.css";
 import {
-    IconCalendar,
-    IconFire,
     IconHome,
     IconLocation,
     IconMan,
     IconUserGroup,
     IconWoman
 } from "@arco-design/web-react/icon";
+
 export function TopicArea(props) {
     const data = props.data;
 
-    function joinMoveInRangeDate(){
+    function joinMoveInRangeDate() {
         let st = data.moveInStart.seconds;
         let ed = data.moveInEnd.seconds;
 
-        let stStr = new Date(st *1000).toLocaleDateString();
-        let edStr = new Date(ed *1000).toLocaleDateString();
+        let duration = (ed - st + 1) / (86400 * 30);
 
-        return stStr + " - " + edStr;
+        // Carry up
+        duration = Math.ceil(duration);
+        duration = duration === 1 ? "1 month" : duration + " months";
+
+        let stStr = new Date(st * 1000).toLocaleDateString();
+        //let edStr = new Date(ed * 1000).toLocaleDateString();
+
+        return stStr + " || " + duration;
     }
 
-    const price = "RM " + data.priceMin + " - RM " + data.priceMax;
+    const price = "RM " + data.priceMin + " - RM " + data.priceMax + " / month";
 
-    return(
+    return (
         //topic,type,apartment, moveInRange, bedroomNum.jsx,gender,size,priceRange,phone,images,description,postTimeStamp
 
 
         <div className="topic-area">
             <div className="topic-topic">{data.topic}</div>
+            <div className="important-info">
+                <div className="priceRange">
+                    {price}</div>
+                <div className="moveInRange">
+                    {joinMoveInRangeDate()}</div>
+            </div>
             <div className="tags">
-                <div className="apartment" >
-                    <IconLocation className={"room-topic-icon"} />
+
+
+                <div className="apartment">
+                    <div className="icon-con">
+                        <IconLocation className={"room-topic-icon"}/>
+                    </div>
                     {data.apartment}
                 </div>
-                {/*<div className="type" >{data.type}</div>*/}
-                <div className="priceRange">
-                    <IconFire className={"room-topic-icon"} />
-                    {price}</div>
-                <div className="size" >
-                    <IconHome className={"room-topic-icon"}  />
+
+                <div className="size">
+                    <div className="icon-con">
+                        <IconHome className={"room-topic-icon"}/>
+                    </div>
                     {data.size}</div>
                 <div className="bedroom-num-r">
-                    <IconUserGroup className={"room-topic-icon"}/>
-                    {data.bedroomNum} Bedrooms</div>
-                { data.gender === "Male" &&
-                    <div className="gender" >
-                        <IconMan className={"room-topic-icon"}  />
+                    <div className="icon-con">
+                        <IconUserGroup className={"room-topic-icon"}/>
+                    </div>
+                    {data.bedroomNum} Bedrooms
+                </div>
+                {data.gender === "Male" &&
+                    <div className="gender">
+                        <div className="icon-con">
+                            <IconMan className={"room-topic-icon"}/>
+                        </div>
                         {data.gender}</div>
                 }
-                { data.gender === "Female" &&
-                    <div className="gender" >
-                        <IconWoman className={"room-topic-icon"}  />
+                {data.gender === "Female" &&
+                    <div className="gender">
+                        <div className="icon-con">
+                            <IconWoman className={"room-topic-icon"}/>
+                        </div>
                         {data.gender}</div>
                 }
-                { data.gender === "Any" &&
-                    <div className="gender" >
-                        <IconMan className={"room-topic-icon"}  />
-                        <IconWoman className={"room-topic-icon"}  />
+                {data.gender === "Any" &&
+                    <div className="gender">
+                        <div className="icon-con">
+                            <IconMan className={"room-topic-icon"}/>
+                        </div>
+                        <div className="icon-con">
+                            <IconWoman className={"room-topic-icon"}/>
+                        </div>
                         {data.gender}</div>
                 }
-                <div className="moveInRange" >
-                    <IconCalendar className={"room-topic-icon"}  />
-                    {joinMoveInRangeDate()}</div>
+
             </div>
         </div>
     )
