3730a7dabae6708af096d5cf8973ba534c229ccf
Innis <1575270674@qq.com>
2023-01-07 00:12:29 +0800

    feat: add filter clean

--- src/components/home/filter.jsx
+++ src/components/home/filter.jsx
@@ -2,12 +2,12 @@ import {Button, ConfigProvider, DatePicker, InputNumber, Radio, Select, Slider} 
 
 import enUS from "@arco-design/web-react/es/locale/en-US.js";
 import {useState} from "react";
-import {getWhereConditions,filterPriceAndMoveInDate} from "./getWhereConditions.js";
+import {getWhereConditions, filterPriceAndMoveInDate} from "./getWhereConditions.js";
 import {StatusContainer} from "../../StatusContainer.js";
 import {useNavigate} from "react-router-dom";
 import {apartments} from "../../config.js";
 
-export function Filter(){
+export function Filter() {
     const filterCSS = {
         marginTop: "40px",
         width: "250px",
@@ -28,11 +28,11 @@ export function Filter(){
 
 
     const Option = Select.Option;
-    const options2 =apartments;
+    const options2 = apartments;
 
-    const { RangePicker } = DatePicker;
+    const {RangePicker} = DatePicker;
 
-    const [value, setValue] = useState(30);
+    //const [value, setValue] = useState(30);
 
     function formatTooltip(val) {
         return <span>RM {val}</span>;
@@ -61,14 +61,14 @@ export function Filter(){
             apartment: apartment,
             moveInDateRange: moveInDateRange,
             bedroomNum: bedroomNum,
-            gender:gender,
+            gender: gender,
             size: size,
             priceRange: priceRange,
         }
         let queries = getWhereConditions(data);
-        console.log("queries",queries);
+        console.log("queries", queries);
 
-        let simpleQueries =queries[0];
+        let simpleQueries = queries[0];
 
         //console.log(simpleQueries);
 
@@ -76,46 +76,61 @@ export function Filter(){
         let fbStore = StatusContainer.fireBaseStore;
 
         let res;
-        if(simpleQueries.length !== 0){
+        if (simpleQueries.length !== 0) {
             res = await fbStore.query("rooms", simpleQueries);
-        }else{
+        } else {
             res = StatusContainer.currentAllRoomsData;
         }
 
 
-
         StatusContainer.currentSearchRoomsData = filterPriceAndMoveInDate(res, queries[1]);
-        console.log("search result",StatusContainer.currentSearchRoomsData);
+        console.log("search result", StatusContainer.currentSearchRoomsData);
 
         navigate("/home/search");
 
     }
 
-    function typeChange(value){
-        if(value===options1[0]){
+    function typeChange(value) {
+        //console.log(value);
+        if (value === options1[0]) {
             setType(true);
-        }else{
+        } else {
             setType(false);
         }
+    }
+
+
+    function clear() {
+        //setType(null);
+        setApartment(null);
+        setBedroomNum(null);
+        setGender(null);
+        setSize(null);
+        // setMoveInDateRange(null);
+        // setPriceRange(null);
+        //console.log(type);
+        navigate("/home");
     }
 
     /*function onChangeType(value) {
         setType(value);
     }*/
 
-    return(
+    return (
         <div style={filterCSS}>
-           <div className={"post-title"}>
-                <span >Type</span>
-               <RadioGroup options={options1} className={"radios"} onChange={typeChange}/>
-           </div>
+            <div className={"post-title"}>
+                <span>Type</span>
+                <RadioGroup options={options1} className={"radios"} onChange={typeChange} />
+            </div>
 
             <div>
                 <span className={"post-title"}>Apartment</span>
                 <Select placeholder='Please select'
-                        style={{ width: "100%", marginBottom: "10px" }}
+                        style={{width: "100%", marginBottom: "10px"}}
                         allowClear allowCreate
-                        onChange={(value)=>setApartment(value)}>
+                        onChange={(value) => setApartment(value)}
+                        value={apartment}
+                >
                     {options2.map((option, index) => (
                         <Option key={index} value={option}>
                             {option}
@@ -124,13 +139,13 @@ export function Filter(){
                 </Select>
             </div>
 
-            <div  >
+            <div>
                 <span className={"post-title"}>Move in date</span>
                 <ConfigProvider locale={enUS}>
                     <RangePicker
                         style={componentCSS}
                         format='YYYY-MM-DD'
-                        onChange={(value)=>setMoveInDateRange(value)}
+                        onChange={(value) => setMoveInDateRange(value)}
                         disabled
                     />
                 </ConfigProvider>
@@ -142,36 +157,44 @@ export function Filter(){
                     mode='button'
                     defaultValue={3}
                     style={componentCSS}
-                    onChange={(value)=>setBedroomNum(value)}
+                    onChange={(value) => setBedroomNum(value)}
+                    value={bedroomNum}
                 />
             </div>
 
-            <div >
+            <div>
                 <span className={"post-title"}>Gender:</span>
-                <RadioGroup options={['Male', "Female","Any"]} className={"radios"}
-                            style={componentCSS} onChange={(value)=>setGender(value)} />
+                <RadioGroup options={['Male', "Female", "Any"]} className={"radios"}
+                            style={componentCSS} onChange={(value) => setGender(value)} value={gender}/>
             </div>
 
-            <div >
+            <div>
                 <span className={"post-title"}>Size:</span>
                 <RadioGroup options={["Small", "Medium", "Master"]} className={"radios"}
-                            style={componentCSS} onChange={(value)=>setSize(value)} />
+                            style={componentCSS} onChange={(value) => setSize(value)} value={size}/>
             </div>
 
-            <div >
+            <div>
                 <span className={"post-title"}>Price:</span>
                 <Slider
                     range
-                        // value={value}
-                        onChange={(value)=>setPriceRange(value)}
-                        defaultValue={[0, 2000]}
-                        max={5000}
+                    // value={value}
+                    onChange={(value) => setPriceRange(value)}
+                    defaultValue={[0, 2000]}
+                    max={5000}
                     formatTooltip={formatTooltip}
                     disabled
                 />
             </div>
+            <div style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+            }}>
+                <Button type="primary" style={{width: 100, margin: "10px auto"}} onClick={search}>Search</Button>
+                <Button type="secondary" style={{width: 100, margin: "10px auto"}} onClick={clear}>Clear</Button>
+            </div>
 
-            <Button type="primary" style={{width:100,margin:"10px auto",display:"block"}} onClick={search}>Search</Button>
         </div>
     )
 }
