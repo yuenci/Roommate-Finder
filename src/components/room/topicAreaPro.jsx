dadf4bd904727750b123c68122c7d2218e629ab0
Innis <1575270674@qq.com>
2023-01-23 19:05:40 +0800

    feat: share buttons

--- src/components/room/topicAreaPro.jsx
+++ src/components/room/topicAreaPro.jsx
@@ -1,36 +1,38 @@
-import { ReactComponent as TranslateIcon } from "./icons/translate.svg";
-import { ReactComponent as ShareIcon } from "./icons/share.svg";
-import { ReactComponent as LoveIcon } from "./icons/love.svg";
+import {ReactComponent as TranslateIcon} from "./icons/translate.svg";
+import {ReactComponent as ShareIcon} from "./icons/share.svg";
+import {ReactComponent as LoveIcon} from "./icons/love.svg";
 import {FBAuth} from "../../firebase/authHandler.js";
 import {FBStore} from "../../firebase/storeHandle.js";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import PubSub from 'pubsub-js';
-
+import {Modal, Message} from '@arco-design/web-react';
+import {IconShareAlt} from '@arco-design/web-react/icon';
 
 export default function TopicAreaPro(props) {
 
-    const  [loved, setLoved] = useState(false);
+    const [loved, setLoved] = useState(false);
     const [translated, setTranslated] = useState(false);
 
     let topicOrigin = props.data.topic;
     let descriptionOrigin = props.data.description;
 
     const [topic, setTopic] = useState(props.data.topic);
-    const {setDescription,description} = props;
+    const {setDescription, description} = props;
 
     let fbStore = new FBStore()
     let email = new FBAuth().auth.currentUser.email;
     let roomID = props.data.roomID;
+    let currentUrl = window.location.href;
 
 
     function loveClick() {
         setLoved(!loved)
-        fbStore.addArrayElement("users", email, "lovedRooms",roomID).then((res) => {
+        fbStore.addArrayElement("users", email, "lovedRooms", roomID).then((res) => {
             console.log(res);
         }).catch(
-            ()=>{
-                fbStore.write("users", {lovedRooms: [roomID]},email).then((res) => {
+            () => {
+                fbStore.write("users", {lovedRooms: [roomID]}, email).then((res) => {
                     console.log(res);
                 })
             }
@@ -40,10 +42,10 @@ export default function TopicAreaPro(props) {
     function translateClick() {
         setTranslated(!translated)
 
-        if(translated){
+        if (translated) {
             setTopic(topicOrigin)
             setDescription(descriptionOrigin)
-            PubSub.publish('translate', { message: 'translateToEnglish' });
+            PubSub.publish('translate', {message: 'translateToEnglish'});
             return;
         }
 
@@ -56,7 +58,7 @@ export default function TopicAreaPro(props) {
             setDescription(r.data.description)
         })
 
-        PubSub.publish('translate', { message: 'translateToChinese' });
+        PubSub.publish('translate', {message: 'translateToChinese'});
     }
 
     useEffect(() => {
@@ -67,26 +69,73 @@ export default function TopicAreaPro(props) {
         })
     }, [])
 
+    let modalIns = null;
+
+    function goToWebsite(url) {
+        window.open(url, "_blank")
+        modalIns.close()
+    }
+
+    const goToFacebook = () => { goToWebsite(`https://www.facebook.com/sharer/sharer.php?u=${currentUrl}`) }
+    const goToInstagram = () => { goToWebsite("https://www.instagram.com") }
+    const goToTwitter = () => { goToWebsite(`https://twitter.com/intent/tweet?url=${currentUrl}`) }
+    const goToWhatsapp = () => { goToWebsite(`https://api.whatsapp.com/send?text==${currentUrl}`) }
+    const copyLink = () => {
+        navigator.clipboard.writeText(currentUrl).then(
+            () => {
+                Message.success('Copied to clipboard!')
+                modalIns.close()
+            }
+        )
+    }
+
+    function shareClick() {
+        console.log("share")
+        modalIns = Modal.confirm({
+            title: 'Share',
+            icon: <IconShareAlt/>,
+            content: (
+                <div className={"share-container"}>
+                    <div onClick={goToFacebook}>
+                        <img src="../../../public/facebook.png" alt="facebook"/>
+                    </div>
+                    <div onClick={goToInstagram}>
+                        <img src="../../../public/instagram.png" alt="instagram"/>
+                    </div>
+                    <div onClick={goToTwitter}>
+                        <img src="../../../public/twitter.png" alt="twitter"/>
+                    </div>
+                    <div onClick={goToWhatsapp}>
+                        <img src="../../../public/whatsapp.png" alt="whatsapp"/>
+                    </div>
+                    <div onClick={copyLink}>
+                        <img src="../../../public/copy.png" alt="copy"/>
+                    </div>
+                </div>
+            ),
+            footer: null,
+        });
+    }
 
 
     return (
         <div className={"topic-area-pro"}>
-           <div className={"topic-area-pro-left"}>
-               <div className={"topic-area-pro-translate"}>
-                   {
-                          !translated
-                       ? <TranslateIcon className={"topic-area-pro-translate"} onClick={translateClick} />
-                       : <TranslateIcon className={"topic-area-pro-translate-active"} onClick={translateClick} />
-                   }
-               </div>
-               <div className={"topic-area-pro-topic"}>{topic}</div>
-           </div>
+            <div className={"topic-area-pro-left"}>
+                <div className={"topic-area-pro-translate"}>
+                    {
+                        !translated
+                            ? <TranslateIcon className={"topic-area-pro-translate"} onClick={translateClick}/>
+                            : <TranslateIcon className={"topic-area-pro-translate-active"} onClick={translateClick}/>
+                    }
+                </div>
+                <div className={"topic-area-pro-topic"}>{topic}</div>
+            </div>
             <div className={"topic-area-pro-right"}>
-                <ShareIcon className={"topic-area-pro-share"}/>
+                <ShareIcon className={"topic-area-pro-share"} onClick={shareClick}/>
                 {
                     !loved
-                    ? <LoveIcon className={"topic-area-pro-love"} onClick={loveClick} />
-                    : <LoveIcon className={"topic-area-pro-love-active"} onClick={loveClick} />
+                        ? <LoveIcon className={"topic-area-pro-love"} onClick={loveClick}/>
+                        : <LoveIcon className={"topic-area-pro-love-active"} onClick={loveClick}/>
                 }
             </div>
         </div>
